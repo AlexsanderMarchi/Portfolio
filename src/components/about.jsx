4a399ac1a5eba35747302685bc28071037fa4b64
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-02-06 15:03:23 -0300

    About Section Finished

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,84 +8,58 @@ import myself from "../assets/Myself.jpg";
 import random from "../assets/Me.jpg";
 
 function About() {
-  const items = document.querySelectorAll("#apresentation .column");
-
-  const isInViewport = (el) => {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  };
-
-  const run = () =>
-    items.forEach((item) => {
-      if (isInViewport(item)) {
-        item.classList.add("show");
-      }
-    });
-
-  // Events
-  window.addEventListener("load", run);
-  window.addEventListener("resize", run);
-  window.addEventListener("scroll", run);
-
   return (
-    <div id="about" className="flex-items">
-      <div className="apresentation flex-items bg-secundary left img-left">
-        <div className="column reverse">
-          <img src={myself} alt="Myself" className="img-left" />
-        </div>
-        <div className="column">
-          <h2>Quem Sou Eu?</h2>
-          <p>
-            Olá, Me chamo Alexsander, 23 anos. Nerd, amante de cinema e música
-            boa, atualmente, estou me graduando na instituição Estácio em
-            Análise e Desenvolvimento de Sistemas.
-          </p>
-          <p>
-            Ja desenvolvi em JavaScript, React, React Native, C++, Node.js e
-            SQL, e a cada dia aprendendo mais.
-          </p>
-        </div>
-      </div>
-      <div className="apresentation flex-items bg-grey right img-right">
-        <div className="column reverse">
-          <h2>O que eu busco?</h2>
-          <p>
-            Ja tendo concluído o curso Técnico em Desenvolvimento de Sistemas no
-            Senai São José, estou cada dia lutando mais para conseguir me
-            desenvolver profissionalmente e como pessoa.
-          </p>
-          <p>
-            Eu busco constantemente desafios que me levem a aprender algo novo.
-          </p>
-        </div>
-        <div className="column">
-          <img src={random} alt="Myself" className="img-right" />
+    <div id="about" className="flex-items py-2">
+      <div className="container flex-items">
+        <h1>Sobre Mim</h1>
+        <div className="underline"></div>
+        <div className="about-container">
+          <img src={myself} alt="Myself" className="img1" />
+          <div className="bio1">
+            <p>
+              Olá, Me chamo Alexsander, 23 anos. Nerd, amante de cinema e música
+              boa, atualmente, estou me graduando na instituição Estácio em
+              Análise e Desenvolvimento de Sistemas. Ja desenvolvi em
+              JavaScript, React, React Native, C++, Node.js e SQL, e a cada dia
+              aprendendo mais.
+            </p>
+          </div>
+          <div className="bio2">
+            <p>
+              Ja tendo concluído o curso Técnico em Desenvolvimento de Sistemas
+              no Senai São José, estou cada dia lutando mais para conseguir me
+              desenvolver profissionalmente e como pessoa. Eu busco
+              constantemente desafios que me levem a aprender algo novo.
+            </p>
+          </div>
+          <img src={random} alt="Myself" className="img2" />
         </div>
       </div>
-      {/* <Element name="section1"></Element> */}
-      {/* <a href="#">
-                <Lottie animationData={scrollAnimation}
-                    loop
-                    autoplay
-                    speed={5}
-                    style={{ width: '200px', height: '100px' }} />
-            </a> */}
-
-      {/* <Link to="section2" spy={true} smooth={true} duration={500} offset={-25} delay={100}>
-                <Lottie animationData={scrollAnimation}
-                    loop
-                    autoplay
-                    speed={5}
-                    style={{ width: '200px', height: '100px' }} />
-            </Link> */}
     </div>
   );
 }
 
 export default About;
+
+{
+  /* <Element name="section1"></Element> */
+}
+{
+  /* <a href="#">
+          <Lottie animationData={scrollAnimation}
+              loop
+              autoplay
+              speed={5}
+              style={{ width: '200px', height: '100px' }} />
+      </a> */
+}
+
+{
+  /* <Link to="section2" spy={true} smooth={true} duration={500} offset={-25} delay={100}>
+          <Lottie animationData={scrollAnimation}
+              loop
+              autoplay
+              speed={5}
+              style={{ width: '200px', height: '100px' }} />
+      </Link> */
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,7 +12,7 @@ function Home() {
     <>
       <Header />
 
-      {/* <About /> */}
+      <About />
 
       {/* <Experience />
 
