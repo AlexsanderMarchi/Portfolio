0c4e1191b8a4b5074dc5e49eb2cefd162213e4e1
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-02-09 01:00:38 -0300

    Experience section layout finish

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,43 +6,90 @@ import senai from "../assets/senaiLogo.jpg";
 import estacio from "../assets/estacioLogo.png";
 import Lottie from "lottie-react";
 import scrollAnimation from "../assets/scroll-white.json";
+import {
+  FaReact,
+  FaNodeJs,
+  FaHtml5,
+  FaCss3Alt,
+  FaRegCalendarAlt,
+} from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiCypress } from "react-icons/si";
 // import starWars from '../assets/starWars.json';
+import { FaRegCalendarCheck } from "react-icons/fa";
 
 function Experience() {
   const experienceList = [
     {
       id: 1,
-      imagem: senai,
-      data: "maio de 2021 - dezembro de 2023",
-      competencias:
-        "JavaScript - HTML5, CSS3 - React - React Native - C++ - Node.js - SQL - Modelagem de Sistemas - Cypress.io",
+      imagem: estacio,
+      data: [<FaRegCalendarAlt />, " Fevereiro de 2024 - Agosto de 2026"],
+      competencias: [
+        <FaReact />,
+        <IoLogoJavascript />,
+        <FaHtml5 />,
+        <FaCss3Alt />,
+        <FaNodeJs />,
+        <BiLogoPostgresql />,
+        <SiCypress />,
+      ],
       descricao:
-        "Sistema de Ensalamento, com CRUD(modelo MVC e Facade) e quadro de horários. Foi utilizado React, Node.js e SQL",
-      title: "Senai SA",
-      github: "https://github.com/PedroHGaspar/Senai-S-A",
+        "Criação de um Streaming voltado para Pets, sendo desenvolvido em React e Node.js na parte de API.",
+      title: "Graduação",
+      tag: "tag tag-estudos",
+      tag_type: "Currículo",
     },
     {
       id: 2,
-      imagem: estacio,
-      data: "maio de 2021 - dezembro de 2023",
-      competencias:
-        "JavaScript - HTML5, CSS3 - React - React Native - C++ - Node.js - SQL - Modelagem de Sistemas - Cypress.io",
+      imagem: senai,
+      data: [<FaRegCalendarCheck />, " Maio de 2021 - Dezembro de 2023"],
+      competencias: [
+        <FaReact />,
+        <IoLogoJavascript />,
+        <FaHtml5 />,
+        <FaCss3Alt />,
+        <FaNodeJs />,
+        <BiLogoPostgresql />,
+        <SiCypress />,
+      ],
       descricao:
-        "Criação de um Streaming voltado para Pets, sendo desenvolvido em React e Node.js na parte de API.",
-      title: "Petflix",
-      github: "https://github.com/AlexsanderMarchi/petFlix",
+        "Na instituição Senai de São José, me formei como Técnico em Desenvolvimento de Sistemas, aprendendo programação de baixo nível assim como de alto nível, Banco de Dados, projetos em equipe, além de Modelagem e Testes de Sistemas.",
+      title: "Desenvolvimento de Sistema",
+      tag: "tag tag-estudos",
+      tag_type: "Currículo",
+    },
+    {
+      id: 3,
+      imagem: senai,
+      data: [<FaRegCalendarCheck />, " Maio de 2021 - Dezembro de 2023"],
+      competencias: [
+        <FaReact />,
+        <IoLogoJavascript />,
+        <FaHtml5 />,
+        <FaCss3Alt />,
+        <FaNodeJs />,
+        <BiLogoPostgresql />,
+        <SiCypress />,
+      ],
+      descricao:
+        "Na instituição Senai de São José, me formei como Técnico em Desenvolvimento de Sistemas, aprendendo programação de baixo nível assim como de alto nível, Banco de Dados, projetos em equipe, além de Modelagem e Testes de Sistemas.",
+      title: "Desenvolvimento de Sistema",
+      tag: "tag tag-estudos",
+      tag_type: "Currículo",
     },
   ];
 
   return (
     <div id="experience" className="py-3">
-      <div className="container flex-items">
+      <div className="flex-items">
         <h1>Experiências</h1>
         <div className="underline"></div>
         <ul>
           {Object.values(experienceList).map((experience) => (
             <li key={experience.id}>
-              <div className="experience-container">
+              <div className="experience-container py-2">
+                <p className={experience.tag}>{experience.tag_type}</p>
                 <div className="flex-items">
                   <img src={experience.imagem} />
                   <div id="info" className="flex-items">
@@ -51,14 +98,10 @@ function Experience() {
                     <p>{experience.descricao}</p>
                   </div>
                 </div>
+                <div className="icons-competencias py-2">
+                  {experience.competencias}
+                </div>
                 <p>{experience.data}</p>
-                <p> Competências: {experience.competencias}</p>
-                <p>
-                  GIT HUB: <br />
-                  <a href={experience.github} target="_blank">
-                    {experience.github}
-                  </a>
-                </p>
               </div>
             </li>
           ))}
